012438a15ecf5255dc0a4706018f5247fbd9356e
Alexandr Urechin <sannyok@gmail.com>
2018-05-25 12:15:00 +0300

    remove from master

--- dbl_linked_list/lib/dbl_linked_list/version.rb
+++
@@ -1,3 +0,0 @@
-module DblLinkedList
-  VERSION = "0.1.0"
-end
--- dbl_linked_list/lib/node.rb
+++
@@ -1,9 +0,0 @@
-class Node
-  attr_accessor :data, :next, :previous
-
-  def initialize(data)
-    @data = data
-    @next = nil
-    @previous = nil
-  end
-end
